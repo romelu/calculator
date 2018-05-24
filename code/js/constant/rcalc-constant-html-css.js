0f8d9c90e5ae312b3f793d8530280237b85a4240
Zazo <daniel.zazo@storaenso.com>
2018-05-24 21:17:00 +0200

    BG-21 - creating spaces betweeen categories in files + move constant SUFFIX_PX

--- code/js/constant/rcalc-constant-html-css.js
+++ code/js/constant/rcalc-constant-html-css.js
@@ -6,6 +6,7 @@ const WIDTH_SMALL_93_PX = 93;
 const WIDTH_SMALL_125_PX = 125;
 const HEIGHT_SMALL_30_PX = 30;
 const HEIGHT_SMALL_25_PX = 25;
+
 // size default in px
 const FONT_SIZE_DEFAULT_40_PX = 40;
 const FONT_SIZE_DEFAULT_17_PX = 17;
@@ -14,6 +15,7 @@ const WIDTH_DEFAULT_186_PX = 186;
 const WIDTH_DEFAULT_250_PX = 250;
 const HEIGHT_DEFAULT_60_PX = 60;
 const HEIGHT_DEFAULT_55_PX = 55;
+
 // size large in px
 const FONT_SIZE_LARGE_70_PX = 70;
 const FONT_SIZE_LARGE_30_PX = 30;
@@ -21,8 +23,7 @@ const WIDTH_LARGE_100_PX = 100;
 const WIDTH_LARGE_305_PX = 305;
 const WIDTH_LARGE_410_PX = 410;
 const HEIGHT_LARGE_100_PX = 100;
-// adds to a value px, making it work to change atributes set in px
-const SUFFIX_PX = "px";
+
 // colors
 const COLOR_BLACK = "#000000";
 const COLOR_WHITE = "#ffffff";﻿
