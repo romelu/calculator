5c08b63d01ca695bfd7c5ad806536cbaac0388cd
romelu <zak.romelu@gmail.com>
2018-06-09 22:18:58 +0200

    misspelling corections only

--- code/js/constant/rcalc-constant-string-utility.js
+++ code/js/constant/rcalc-constant-string-utility.js
@@ -4,5 +4,5 @@ const DOT = ".";
 const QUESTION_MARK = "?"
 const EQUALS_SIGN = "="
 
-// adds to a value px, making it work to change atributes set in px
+// adds to a value px, making it work to change attributes set in px
 const SUFFIX_PX = "px";
