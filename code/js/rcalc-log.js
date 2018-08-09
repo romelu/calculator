5108b37a9a15e6aff2056b97d91e985da8bf18d1
romelu <you@example.com>
2018-08-09 11:20:02 +0100

    created new function changeLineColor, which makes every second line light grey

--- code/js/rcalc-log.js
+++ code/js/rcalc-log.js
@@ -1,12 +1,16 @@
+var lineCounter = 0;
+
 function setLogDisplayLine() {
+  lineCounter++;
   var parentDiv = document.getElementById("id-container-log-display");
   var paragraph = document.createElement("p");
-  paragraph.style.backgroundColor = "#ffffff";
+  paragraph.style.backgroundColor = "#adadad";
   var text = document.createTextNode(leftSideOfEqualSignGlobal + "=" + resultGlobal);
   paragraph.appendChild(text);
   parentDiv.appendChild(paragraph);
   var nextToLastChild = parentDiv.lastChild.previousSibling;
   parentDiv.removeChild(nextToLastChild);
+  changeLineColor(paragraph);
 }
 
 function setDisplayLineErrorCase(leftSideOfEqualSignGlobal) {
@@ -16,4 +20,10 @@ function setDisplayLineErrorCase(leftSideOfEqualSignGlobal) {
   var text = document.createTextNode(leftSideOfEqualSignGlobal + " ERROR");
   paragraph.appendChild(text);
   parentDiv.appendChild(paragraph);
+}
+
+function changeLineColor(paragraph) {
+  if (lineCounter % 2 == 1) {
+    paragraph.style.backgroundColor = "#ffffff";
+  }
 }
