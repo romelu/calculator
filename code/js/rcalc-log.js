62d6af67911368f112c77c035f4d95e262f500cd
romelu <you@example.com>
2018-08-10 15:50:33 +0100

    logic of log display changed, latest log goes to the top of the screen not as the last one on the botom

--- code/js/rcalc-log.js
+++ code/js/rcalc-log.js
@@ -2,33 +2,33 @@ var lineCounter = 0;
 
 function setLogDisplayLine() {
   lineCounter++;
-  var parentDiv = document.getElementById("id-container-log-display");
+  var parentDiv = document.getElementById("id-log-display");
   var paragraph = document.createElement("p");
   var text;
-  var nextToLastChild;
+  var nextToFirstChild;
   paragraph.style.backgroundColor = "#adadad";
   paragraph.setAttribute("class", "rcalc-log-display-line");
   text = document.createTextNode(leftSideOfEqualSignGlobal + "=" + resultGlobal);
   paragraph.appendChild(text);
-  parentDiv.appendChild(paragraph);
-  nextToLastChild = parentDiv.lastChild.previousSibling;
-  parentDiv.removeChild(nextToLastChild);
+  parentDiv.insertBefore(paragraph, parentDiv.firstChild);
+  nextToFirstChild = parentDiv.firstChild.nextSibling;
+  parentDiv.removeChild(nextToFirstChild);
   changeLineColor(paragraph);
 }
 
 function setDisplayLineErrorCase(leftSideOfEqualSignGlobal) {
-  var parentDiv = document.getElementById("id-container-log-display");
+  var parentDiv = document.getElementById("id-log-display");
   var paragraph = document.createElement("p");
   var text;
   paragraph.style.backgroundColor = "#ff6666";
   paragraph.setAttribute("class", "rcalc-log-display-line");
-  text = document.createTextNode(leftSideOfEqualSignGlobal + " ERROR");
+  text = document.createTextNode(leftSideOfEqualSignGlobal + "ERROR");
   paragraph.appendChild(text);
-  parentDiv.appendChild(paragraph);
+  parentDiv.insertBefore(paragraph, parentDiv.firstChild);
 }
 
 function changeLineColor(paragraph) {
   if (lineCounter % 2 == 1) {
-    paragraph.style.backgroundColor = "#ffffff";
+    paragraph.style.backgroundColor = "#FFFFFF";
   }
 }
